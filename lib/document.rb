764b1d3628712abb6c722d559f8276987cf41bf2
Guillermo Álvarez <guillermo@cientifico.net>
2008-12-18 15:34:49 +0100

    Translate chars that does not exists in iso15 to chars that exists in iso15, and ignore all the others

--- lib/document.rb
+++ lib/document.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module Groff
   class Document
     
@@ -38,8 +39,9 @@ module Groff
     
   private
     def filter_iconv
-      @output = Iconv.iconv("iso-8859-15","utf-8", @output)
-    rescue Iconv::IllegalSequence
+      normalize
+      @output = Iconv.iconv("iso-8859-15//ignore","utf-8", @output)
+    rescue Iconv::IllegalSequence => e
       @output = Iconv.iconv("iso-8859-1","utf-8", @output)
     end
     
@@ -53,6 +55,39 @@ module Groff
     
     def ps_command
       "groff #{@options[:params]} "
+    end
+
+    def normalize
+      @output.tr!('“”–','""-')
+      @output.gsub!('º','o')
+      @output.gsub!('´','`')
+      @output.gsub!('ª','a')
+      @output.gsub!('ŕ','r')
+      @output.gsub!(/[àâãäåāă]/,    'a')
+      @output.gsub!(/æ/,            'ae')
+      @output.gsub!(/[ďđ]/,          'd')
+      @output.gsub!(/[çćčĉċ]/,       'c')
+      @output.gsub!(/[èêëēęěĕė]/,   'e')
+      @output.gsub!(/ƒ/,             'f')
+      @output.gsub!(/[ĝğġģ]/,        'g')
+      @output.gsub!(/[ĥħ]/,          'h')
+      @output.gsub!(/[ììîïīĩĭ]/,    'i')
+      @output.gsub!(/[įıĳĵ]/,        'j')
+      @output.gsub!(/[ķĸ]/,          'k')
+      @output.gsub!(/[łľĺļŀ]/,       'l')
+      @output.gsub!(/[ñńňņŉŋ]/,      'n')
+      @output.gsub!(/[òôõöøōőŏŏ]/,  'o')
+      @output.gsub!(/œ/,            'oe')
+      @output.gsub!(/ą/,             'q')
+      @output.gsub!(/[ŕřŗ]/,         'r')
+      @output.gsub!(/[śšşŝș]/,       's')
+      @output.gsub!(/[ťţŧț]/,        't')
+      @output.gsub!(/[ùûüūůűŭũų]/,  'u')
+      @output.gsub!(/ŵ/,             'w')
+      @output.gsub!(/[ýÿŷ]/,         'y')
+      @output.gsub!(/[žżź]/,         'z')
+      @output.gsub!(/\s+/,           ' ')
+      @output
     end
   end
 end
